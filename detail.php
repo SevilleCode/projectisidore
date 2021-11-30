047f6c9340b4ad2488cbe0a61f23f7e908faa89f
SevilleCode <51100173+SevilleCode@users.noreply.github.com>
2021-11-29 21:30:51 -0500

    Added detail page
    Added a detail page off the category page. Clicking a link on the category page will now take you to a separate detail page.

--- detail.php
+++ detail.php
@@ -40,7 +40,7 @@ if (isset($_GET['pageNum_latestEntries'])) {
 }
 $startRow_latestEntries = $pageNum_latestEntries * $maxRows_latestEntries;
 
-$query_latestEntries = "SELECT * FROM sources ORDER BY id DESC";
+$query_latestEntries = "SELECT * FROM sources ORDER BY Posting DESC";
 $query_limit_latestEntries = sprintf("%s LIMIT %d, %d", $query_latestEntries, $startRow_latestEntries, $maxRows_latestEntries);
 $latestEntries = $myData->query($query_limit_latestEntries) or die($myData->error);
 $row_latestEntries = $latestEntries->fetch_assoc();
@@ -66,27 +66,79 @@ $query_nav = "SELECT DISTINCT Category FROM sources";
 $nav = $myData->query($query_nav) or die($myData->error);
 $row_nav = $nav->fetch_assoc();
 $totalRows_nav = $nav->num_rows;
+
+$colname_details = "-1";
+if (isset($_GET['Source'])) {
+  $colname_details = $_GET['Source'];
+}
+$query_details = sprintf("SELECT * FROM sources WHERE Source = %s ORDER BY Source ASC", GetSQLValueString($colname_details, "text"));
+$details = $myData->query($query_details) or die($myData->error);
+$row_details = $details->fetch_assoc();
+$totalRows_details = $details->num_rows;
+
+$query_Recordset1 = "SELECT site_id FROM name_id";
+$Recordset1 = $myData->query($query_Recordset1) or die($myData->error);
+$row_Recordset1 = $Recordset1->fetch_assoc();
+$totalRows_Recordset1 = $Recordset1->num_rows;
 ?>
-<?php include('categoryheader.php') ?> 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <meta name="description" content="<?php echo strip_tags($row_details['Description']); ?>">
+<title><?php echo $row_Recordset1['site_id']; ?> | <?php echo $row_details['Source']; ?></title>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap-4.4.1.css" rel="stylesheet">
+
+  </head>
+  <body class="<?php echo $row_details['Category']; ?>">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+       <a class="navbar-brand" href="../">Compendium Catholica</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+       <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+             <li class="nav-item">
+                <a class="nav-link" href="../">Home <span class="sr-only">(current)</span></a>
+             </li>
+			  <?php do { ?>
+			      <li class="nav-item"><a class="nav-link" id="<?php echo $row_nav['Category']; ?>" href="../category/<?php echo $row_nav['Category']; ?>"><?php echo $row_nav['Category']; ?></a></li>
+			  <?php } while ($row_nav = $nav->fetch_assoc()); ?>
+            
+          </ul>
+          <form action="../results.php" method="post" class="form-inline my-2 my-lg-0">
+             <input name="search" id="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+       </div>
+    </nav>
+
 
     <div class="jumbotron jumbotron-fluid text-center">
-       <h1 class="display-4">Project Isidore&nbsp;</h1>
-       <p class="lead">Basic CMS Framework&nbsp;</p>
+       <h1 class="display-4">Project Isidore</h1>
+       <p class="lead">Basic CMS Framework</p>
        <hr class="my-4">
-		<h2><?php echo $row_categories['Category']; ?></h2>
-       <?php do { ?>
-       <p><a href="../detail/<?php echo urlencode($row_categories['Source'])?>"><?php echo ($row_categories['Source'])?></a><br>
-         <?php echo $row_categories['Posting']; ?><br>
-         <?php echo $row_categories['Description']; ?>
-         <?php } while ($row_categories = $categories->fetch_assoc()); ?>
-       </p>
 		
+		
+       <p><?php echo $row_details['Source']; ?><br>
+         <a href="<?php echo $row_details['URL']; ?>" target="new"><?php echo $row_details['URL']; ?></a><br>
+         <?php echo $row_details['Posting']; ?>	     </p>
+		   <p><?php echo $row_details['Description']; ?></p>
     </div>
     <div class="container">
+		
       
        <div class="row">
+		   <div class="col-md-8 text-left" id="list">
+      </div>
+		  </div>
          
        <div class="row">
+          <div class="row">
           <div class=" col-md-4"><h3>Daily Readings</h3> <script src="//rss.bloople.net/?url=https%3A%2F%2Fevangelizo.org%2Frss%2Fv2%2Fevangelizo_rss-tra.xml&detail=50&limit=2&type=js"></script> </div>
           <div class="col-md-4 "><h3>Listen to Gregorian Chant and Renaissance Polyphony on SacredMusic.fm!</h3> <script src="https://embed.radio.co/player/481be90.js"></script>	</div>
          <div class="col-md-4 "><h3>Latest Entries</h3>
@@ -94,6 +146,7 @@ $totalRows_nav = $nav->num_rows;
           <li><a href="<?php echo $row_latestEntries['URL']; ?>" target="_blank"><?php echo $row_latestEntries['Source']; ?></a><br>
             <?php echo $row_latestEntries['Posting']; ?></li>
 			   <?php } while ($row_latestEntries = $latestEntries->fetch_assoc()); ?></ul>
+         </div>
          </div>
        </div>
       
@@ -105,4 +158,8 @@ $latestEntries->free();
 $categories->free();
 
 $nav->free();
+
+$details->free();
+
+$Recordset1->free();
 ?>
